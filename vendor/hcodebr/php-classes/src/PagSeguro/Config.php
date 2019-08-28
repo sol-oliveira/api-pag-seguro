b010d49d87b1a57539fa1d59fa6d0f70c4cbd48a
sol <sol@peexell.com.br>
2019-08-28 18:03:45 -0300

    Opções de Parcelamento

--- vendor/hcodebr/php-classes/src/PagSeguro/Config.php
+++ vendor/hcodebr/php-classes/src/PagSeguro/Config.php
@@ -19,8 +19,10 @@ class Config {
     
     const SANDBOX_URL_NOTIFICATION = "https://ws.sandbox.pagseguro.uol.com.br/v3/transactions/notifications/";
 	const PRODUCTION_URL_NOTIFICATION =	"https://ws.pagseguro.uol.com.br/v3/transactions/notifications/";
+   
     const MAX_INSTALLMENT_NO_INTEREST = 10;
     const MAX_INSTALLMENT = 10;
+   
     const NOTIFICATION_URL = "http://www.html5dev.com.br/payment/notification";
     
     public static function getAuthentication():array
