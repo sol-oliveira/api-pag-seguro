6c152ad473c29357ba0ef903c5d1016125dc6cc6
sol <sol@peexell.com.br>
2019-08-29 15:06:27 -0300

    classe Phone.php

--- vendor/hcodebr/php-classes/src/PagSeguro/Phone.php
+++ vendor/hcodebr/php-classes/src/PagSeguro/Phone.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace Hcode\PagSeguro;
+
 use Exception;
 use DOMDocument;
 use DOMElement;
@@ -10,7 +12,8 @@ class Phone {
 	private $areaCode;
 	private $number;
 	
- /*	public function __construct(int $areaCode, int $number)
+
+	public function __construct(int $areaCode, int $number)
 	
 	{
 		if (!$areaCode || $areaCode < 11 || $areaCode > 99)
@@ -22,19 +25,26 @@ class Phone {
 			
 			throw new Exception("Informe o número do telefone.");
 		}
+		
 		$this->areaCode = $areaCode;
-		$this->number = $number;
+				$this->number = $number;
 	}
+	
 	public function getDOMElement():DOMElement
 	{
 	
 		$dom = new DOMDocument();
+		
 		$phone = $dom->createElement("phone");
 		$phone = $dom->appendChild($phone);
+		
 		$areaCode = $dom->createElement("areaCode", $this->areaCode);
 		$areaCode = $phone->appendChild($areaCode);
+		
 		$number = $dom->createElement("number", $this->number);
 		$number = $phone->appendChild($number);
+		
 		return $phone;
-	} */
+	} 
+
 }
